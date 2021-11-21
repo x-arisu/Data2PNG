0ab246e1411e3e5876828b159f58b96f23f78e5e
Victor Bilbao <victor.pasini.bilbao@gmail.com>
2021-11-21 00:55:58 -0300

    refactor code and make it use .txt instead of .data

--- png2data.py
+++ png2data.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 from PIL import Image
 from sys import argv
+
 def main():
-  outputFile = "out.data"
-  if (len(argv) > 2):
-    outputFile = argv[2]
-  out = open(outputFile, 'wb')
-  img = Image.open(argv[1])
-  pixels = list(img.getdata())
-  data = []
-  print("Collecting data...")
-  for i in range(len(pixels)):
-    for x in range(3):
-      data.append(pixels[i][x])
-  print("Data collected! Cleaning...")
-  dataFlag = False
-  endDataIndex = len(data) - 1
-  while(not dataFlag):
-    if (data[endDataIndex] == 0):
-      del data[endDataIndex]
-      endDataIndex -= 1
-    else:
-      dataFlag = True
-  out.write(bytes(data))
-  out.close()
+    
+    output = argv[2] if (len(argv) > 2) else "out.txt"
+    
+    with open(output, 'w') as out:
+        
+        #! open image and list its data
+        img = Image.open(argv[1])
+        pixels = list(img.getdata())
+        
+        print("Collecting data...")
+        
+        #! iterate through each color channel in each pixel and append its value to a list
+        #! only use RGB not RGBA
+        data = [x for pixel in pixels for x in pixel[:3]]
+        
+        print("Data collected! Cleaning...")
+        
+        #! this will remove trailing 0's from the data
+        #* example: data = [1, 2, 3, 4, 0, 0]
+        #*          data = [1, 2, 3, 4]
+        while data[-1] == 0:
+            del data[-1]
+        
+        #! join the data into a string separated by commas
+        out.write(",".join(map(str, data)))
+
 if __name__ == '__main__':
-  main()
+    main()
